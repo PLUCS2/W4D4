34292a6a241acd93b04da04d16195df3b7723482
App Academy Student <aa@Apps-Mac-mini-9.local>
2019-09-05 13:25:57 -0400

    Complete User model and controller

---
+++ music_app/app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController 
+
+  def new
+    render :new
+  end 
+
+  def create 
+    #debugger
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    #debugger 
+    if user 
+      # login!(user)
+      redirect_to user_url(user)
+    else 
+      flash.now[:errors] = [ "that login information was incorrect" ]
+      render :new 
+    end 
+  end 
+
+  def destroy 
+    #debugger
+    logout!(@user)
+    redirect_to new_session_url
+  end 
+
+end  
